08c88d87960e6e19083cf14c79faa3700053f16a
“Navneet <navneet.ranjan11@gmail.com>
2020-08-23 13:34:43 +0000

    Updated the App.js file with different message

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload by Navneet
+          Edit <code>src/App.js</code> and save to reload by Navneet and DevOps course team
         </p>
         <a
           className="App-link"
